16b6226ebb6b4a74bd8ca2736a2f5b3e4a6549b1
daniel <danieldiogo@live.com>
2024-02-24 19:25:50 -0300

    Caminho do banco

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,23 +8,26 @@ namespace Api_AIKO.Data
     public class ApplicationDbContext : IdentityDbContext
     {
         private readonly IServiceProvider _serviceProvider;
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+           : base(options)
         {
-            string parentDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)?.Parent?.Parent?.Parent?.Parent?.FullName;
-
-            if (!string.IsNullOrEmpty(parentDirectory))
-            {
-                // Combinar o caminho do diretório pai com a pasta "Banco"
-                string dataDirectory = Path.Combine(parentDirectory, "Banco");
-
-                // Definir o diretório de dados
-                AppDomain.CurrentDomain.SetData("DataDirectory", dataDirectory);
-            }
-
+            EnsureDataDirectoryExists();
         }
 
+        private void EnsureDataDirectoryExists()
+        {
+            // Obter o caminho do diretório "Banco"
+            string dataDirectory = Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)?.Parent?.Parent?.Parent?.Parent?.FullName, "Banco");
+
+            // Verificar se o diretório existe, e criar se não existir
+            if (!Directory.Exists(dataDirectory))
+            {
+                Directory.CreateDirectory(dataDirectory);
+            }
+
+            // Definir o diretório de dados para o diretório "Banco"
+            AppDomain.CurrentDomain.SetData("DataDirectory", dataDirectory);
+        }
         public DbSet<Line> Lines { get; set; } = default!;
         public DbSet<Stop> stops { get; set; } = default!;
         public DbSet<Vehicle> vehicles { get; set; } = default!;
